b9f2ad019cf4508111ad0afcbb20caf31a7c35eb
1137830424@qq.com <1137830424@qq.com>
2018-04-25 17:04:13 +0800

    Xu
    FTPDemo

---
+++ FTPDemo/app/src/main/java/com/my/ftpdemo/MainActivity.java
@@ -0,0 +1,64 @@
+package com.my.ftpdemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        String ip = getIp();
+        if(TextUtils.isEmpty(ip)){
+            textView.setText("获取不到IP，请连接网络");
+        }else{
+            String str = "请在IE浏览器上输入网址访问人脸识别图片\n" +
+                    "ftp://"+ip+":2221\n" +
+                    "账号:admin\n" +
+                    "密码:123456";
+            textView.setText(str);
+        }
+
+        startService(new Intent(this, FtpService.class));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(new Intent(this, FtpService.class));
+    }
+
+
+    public String getIp(){
+        //获取wifi服务
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        //判断wifi是否开启
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        int ipAddress = wifiInfo.getIpAddress();
+        String ip = intToIp(ipAddress);
+        return ip;
+    }
+
+
+    private String intToIp(int i) {
+
+        return (i & 0xFF ) + "." +
+                ((i >> 8 ) & 0xFF) + "." +
+                ((i >> 16 ) & 0xFF) + "." +
+                ( i >> 24 & 0xFF) ;
+    }
+}
